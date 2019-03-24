53b1921342c71bd8f33266d0891a591d29050d89
unknown <daan.pepping@student.hu.nl>
2019-03-24 21:35:44 +0100

    productpagina

--- src/main/webapp/js/product.js
+++ src/main/webapp/js/product.js
@@ -1,3 +1,14 @@
+function getProductId(id)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == id){return pair[1];}
+       }
+       return(false);
+}
+
 function productInfo(id) {
 	fetch("rest/msg/" + this.id)
 	.then(response => response.json())
